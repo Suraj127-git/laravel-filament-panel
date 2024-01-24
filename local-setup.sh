25c07b13578014f619c2c2cc1bd6f427dbef2629
Suraj Shetty <shetty44444@gmail.com>
2024-01-24 22:20:26 +0530

    local setup

---
+++ local-setup.sh
@@ -0,0 +1,10 @@
+composer require laravel/pint --dev
+composer require laravel/telescope --dev
+composer require laravel/tinker --dev
+php artisan vendor:publish --provider="Laravel\Tinker\TinkerServiceProvider"
+composer require laravel/telescope
+php artisan telescope:install
+composer require barryvdh/laravel-debugbar --dev
+php artisan vendor:publish --provider="Barryvdh\Debugbar\ServiceProvider"
+composer require --dev barryvdh/laravel-ide-helper --with-all-dependencies
+php artisan vendor:publish --provider="Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider" --tag=config
