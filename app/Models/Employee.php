a7fadb5272dd33483e63565372569808bb756c3d
sandeep-bdm <sandeep@bdmdigitech.com>
2024-01-25 13:04:00 +0530

    added form field and date to db

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'country_id',
+    ];
 }
