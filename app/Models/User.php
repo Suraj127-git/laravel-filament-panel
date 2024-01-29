874ff911e1f698ecc055b0e3f422cc38f09b3609
sandeep-bdm <sandeep@bdmdigitech.com>
2024-01-29 15:07:07 +0530

    access updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,4 +65,9 @@ class User extends Authenticatable implements HasTenants
     {
         return $this->teams->contains($tenant);
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+    }
 }
