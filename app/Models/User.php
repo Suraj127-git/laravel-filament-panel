4600c6fd9d1a0f533697c20c7b9458c9ffd5aa1a
sandeep-bdm <sandeep@bdmdigitech.com>
2024-01-29 15:38:50 +0530

    changes for access updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,6 +69,6 @@ class User extends Authenticatable implements HasTenants, FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@gmail.com');
     }
 }
