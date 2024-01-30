ffaa48bc2e502549dc2f77b7c7fb4fc9e8d9e28b
sandeep-bdm <sandeep@bdmdigitech.com>
2024-01-30 18:57:41 +0530

    composer updated

--- app/Logging/logging.php
+++ app/Logging/logging.php
@@ -3,14 +3,8 @@
 use Monolog\Logger;
 use Logtail\Monolog\LogtailHandler;
 
-// Create the $logtail_logger outside the routes
 $logtail_logger = new Logger("logtail-source");
-$logtail_logger->pushHandler(
-    new LogtailHandler(env('TAILLOG_KEY'))
-);
-
+$logtailHandler = new LogtailHandler(env('TAILLOG_KEY'));
 $logtail_logger->error("This is success", ["username" => $username]);
-
-
 
 ?>
